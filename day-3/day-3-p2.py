82c385fc723a80276788e4bc132a80095ebeb22a
Ryan Luker <ryan@luker.dev>
2026-02-11 16:07:37 -0800

    First finished attempt as part 2 day 3

--- day-3/day-3-p2.py
+++ day-3/day-3-p2.py
@@ -1,22 +1,7 @@
 # Look at each joltage battery bank and find the 12 largest values, in order
-# Tactics:
-#   - Look for the biggest value that is not near the end (must be min, 12 away)
-#   - Always prefer the bigger numbers near the front
-#   - After finding the first number, create a subset to find the remaining values
-#   - Might be a good scenario for a recursive design
 
 from fileinput import FileInput
 
-
-def find_the_next_highest_value(current_subset):
-    current_highest = 0
-    for item in current_subset:
-        int_item = int(item)
-        if current_highest < int_item:
-            # New highest item
-            current_highest = int_item
-        # Current highest still biggest
-    return current_highest
 
 with FileInput("example-input.txt") as input:
     total_joltage_amount = 0
@@ -25,16 +10,52 @@ with FileInput("example-input.txt") as input:
         if not joltage_bank:
             # We have hit the bottom of the joltage numbers
             continue
-        # Creates a pre-allocated list 12 items long
-        pre_fab_joltage_bank = [None] * 12
-        current_joltage_bank = list(joltage_bank)
-        for turn in range(0, len(pre_fab_joltage_bank)):
-            # Find the highest outstanding value
-            high_value = find_the_next_highest_value(current_joltage_bank)
-            try:
-                index_high = current_joltage_bank.index(str(high_value))
-            except:
-                print(f"------High value too close to end------")
-            pre_fab_joltage_bank[turn] = high_value
-            current_joltage_bank = current_joltage_bank[index_high+1:]
-        print(f"Finished generating joltage {joltage_bank} - {pre_fab_joltage_bank}")
+        
+        fab_joltage_bank = []
+        list_joltage_bank = list(joltage_bank)
+        # Find the biggest jolt to start at
+        # Note: avoid picking a big number near the end of the bank
+        highest_jolt = 0
+        highest_jolt_index = None
+        for index, jolt in enumerate(list_joltage_bank):
+            # Once we get near the end, avoid picking high jolts
+            if index >= len(list_joltage_bank) - 11:
+                break
+            if highest_jolt < int(jolt):
+                highest_jolt = int(jolt)
+                highest_jolt_index = index
+        
+        # Store the highest jolt and continue
+        fab_joltage_bank.append(highest_jolt)
+
+        # Grab the remainder of the bank for processing
+        subset_joltage_bank = list_joltage_bank[highest_jolt_index + 1:]
+        for jolt in subset_joltage_bank:
+            # Fill up the fab with jolts to start
+            if len(fab_joltage_bank) <= 11:
+                fab_joltage_bank.append(int(jolt))
+                print(fab_joltage_bank)
+                continue
+            # Once full, start to pop small numbers from the front and add numbers to the rear
+            for fab_index, fab_jolt in enumerate(fab_joltage_bank):
+                # Account for the last spot in the fab jolk bank
+                if fab_index == len(fab_joltage_bank) - 1:
+                    # If the last jolt is larger than the last fab jolt, swap
+                    if fab_jolt < int(jolt):
+                        fab_joltage_bank.pop(fab_index)
+                        fab_joltage_bank.append(int(jolt))
+                    break
+                # Check if the next jolt should be used instead
+                if fab_jolt < fab_joltage_bank[fab_index + 1]:
+                    fab_joltage_bank.pop(fab_index)
+                    fab_joltage_bank.append(int(jolt))
+                    print(fab_joltage_bank)
+                    break
+            print(fab_joltage_bank)
+        
+        print(f"Finished generating joltage {joltage_bank} - {fab_joltage_bank}")
+        # Concat finished fab bank into a single string and add it to the total
+        int_fab_joltage_bank = int("".join([str(fab_joltage) for fab_joltage in fab_joltage_bank]))
+        print(f"Final bank: {int_fab_joltage_bank}")
+        total_joltage_amount = total_joltage_amount + int_fab_joltage_bank
+        print(f"Total joltage amount {total_joltage_amount}")
