d23ac7d569e71b5ef7968d59cb1e8bfce9d1611a
Ryan Luker <ryan@luker.dev>
2026-02-18 16:03:39 -0800

    Finish day 5 part 1

--- day-5/day-5.py
+++ day-5/day-5.py
@@ -7,28 +7,45 @@
 # - While constructing this array, deduplicate the ranges.
 # - EG 2-4 would already be partially covered by 3 and 4 above.
 # - When finding out if an ID is "fresh" simply look up the value.
+#
+# Important Note:
+# Lists cannot hold the full amount of fresh ids, instead we need
+# to leverage python range functionality to reduce memory consumption.
 
 from fileinput import FileInput
 
-with FileInput("example-input.txt") as input:
+
+with FileInput("input.txt") as input:
     fresh_database = []
     # Construct the fresh ranges
     for fresh_range in input:
         if fresh_range == "\n":
             break
         fresh_range_params = fresh_range.strip("\n").split("-")
-        fresh_row_range = list(
-            range(
-                int(fresh_range_params[0]), int(fresh_range_params[1])
-            )
+        fresh_row_range = range(
+            int(fresh_range_params[0]), int(fresh_range_params[1]) + 1
         )
+        # Store the python ranges in a list
+        fresh_database.append(fresh_row_range)
 
-        # Loop over the fresh database and find a spot for the new ids
-
-    print(fresh_database)
-
+    fresh_count = 0
     # Look through the food ids for fresh items
     for fresh_item in input:
-        print(fresh_item)
         if fresh_item == "":
             break
+        fresh_id_item = int(fresh_item.strip("\n"))
+        # Find if the fresh_id we are looking up, exists in the database
+        # Note: Leverage range to find out if an item in within
+        for fresh_range in fresh_database:
+            try:
+                fresh_range.index(fresh_id_item)
+                fresh_count = fresh_count + 1
+                # Once we know the fresh item was in one range, skip the rest
+                break
+            except:
+                # Fresh Item isn't within the current range
+                continue
+
+
+    # Final fresh count
+    print(f"Final fresh count: {fresh_count}")
