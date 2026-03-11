4dbcdc793713fa63940e894aa44eb522af83a05d
Ryan Luker <ryan@luker.dev>
2026-03-10 18:22:31 -0700

    Add day-7 skeleton files and folder

---
+++ day-7/day-7.py
@@ -0,0 +1,28 @@
+# Day 7: Laboratories
+# Beam printer
+# 
+# Tactics:
+# 
+#
+# Example:
+# ....S....
+# ....|....
+# ...|^|...
+# ...|.|...
+# ..|^|^|..
+
+
+def construct_beam_array(file_input):
+    pass
+
+
+def calculate_splits(beam_layout):
+    pass
+
+
+with FileInput("example_input.txt") as file_input:
+    beam_layout = construct_beam_array(file_input)
+    print(beam_layout)
+    totals = calculate_splits(beam_layout)
+    print(totals)
+
