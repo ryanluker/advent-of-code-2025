540d732c363c16656930fad7620a583f9b3fe38d
Ryan Luker <ryan@luker.dev>
2026-04-09 10:59:26 -0700

    Add import

--- day-7/day-7.py
+++ day-7/day-7.py
@@ -1,8 +1,8 @@
 # Day 7: Laboratories
 # Beam printer
-# 
+#
 # Tactics:
-# 
+#
 #
 # Example:
 # ....S....
@@ -10,6 +10,9 @@
 # ...|^|...
 # ...|.|...
 # ..|^|^|..
+
+
+from fileinput import FileInput
 
 
 def construct_beam_array(file_input):
@@ -25,4 +28,3 @@ with FileInput("example_input.txt") as file_input:
     print(beam_layout)
     totals = calculate_splits(beam_layout)
     print(totals)
-
