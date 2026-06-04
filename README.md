13d07e052c5952b230dbed89533f2265e05dfb54
Ryan Luker <ryan@luker.dev>
2026-06-04 10:53:22 -0700

    Finish day 8

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Common commands
 
-## Day 1 - 7
+## Basic
 Use uv to consume the proper python version plus deps.
 `uv run day-1.py`
 
-## Day 8+
+## Runner
 Use the runner helper, which cleaned up common file input code
 
 > uv run [runner script] [puzzle day] [run mode (example|full)] [debug mode (True|False)]
--- day-8/day-8-mini.py
+++ day-8/day-8-mini.py
@@ -2,7 +2,7 @@
 # Minimal setup with little imports
 
 junctions = []
-with open('example-input.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     for line in f.readlines():
         x, y, z = (int(k) for k in line.split(','))
         junctions.append((x, y, z))
@@ -10,7 +10,6 @@ with open('example-input.txt', 'r') as f:
 def dist_calc(junc1, junc2):
     return sum((junc1[k] - junc2[k])**2 for k in range(3))
 
-breakpoint()
 union_map = {}
 class Union:
     def __init__(self, members):
@@ -21,7 +20,6 @@ class Union:
         new_members = self.members | other.members
         return Union(new_members)
 
-breakpoint()
 for junc in junctions:
     Union({junc})
 
@@ -31,4 +29,33 @@ for index, point in enumerate(junctions):
         junc = junctions[junc_index]
         all_point_pairs.append((point, junc))
 all_point_pairs.sort(key=lambda pair: dist_calc(pair[0], pair[1]))
-breakpoint()
+
+# Part 1
+# Puzzle includes an iteration cap of 1000
+iteration_cap = 1000
+for index, (point_one, point_two) in enumerate(all_point_pairs):
+    if index == iteration_cap:
+        unions = set()
+        for junc in junctions:
+            unions.add(union_map[junc])
+        unions = list(sorted(unions, key=lambda union: len(union.members), reverse=True))
+        breakpoint()
+        circuit_one = unions[0].members
+        circuit_two = unions[1].members
+        circuit_three = unions[2].members
+        print(f"Largest Circuits: {len(circuit_one)}, {len(circuit_two)}, {len(circuit_three)}") 
+        break
+    
+    # We have found the duplicate entry in the map, continue looping
+    if union_map[point_one] == union_map[point_two]:
+        continue
+    
+    # We want to merge these two maps as their circuits are now connected
+    union = union_map[point_one].union(union_map[point_two])
+
+    # Part 2
+    # If we have reached a one circuit scenario, print the x/y
+    if len(union.members) == len(junctions):
+        print(f"Last connected junctions: {point_one} {point_two}")
+        break
+
